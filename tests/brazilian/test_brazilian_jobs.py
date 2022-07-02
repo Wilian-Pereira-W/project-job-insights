201b582f4a5f957d33508d91757305ae80179c68
Wilian Pereira <wilian_pereira92@outlook.com>
2022-07-01 21:23:59 -0300

    test: da função read_brazilian_file

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,15 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    dados = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    assert (
+        dados[0].keys()
+        == (
+            {
+                "salary": "",
+                "title": "",
+                "type": "",
+            }
+        ).keys()
+    )
